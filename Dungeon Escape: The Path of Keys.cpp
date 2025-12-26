026cd13a00bc98443d9b6c29f1ed1b0f291584ee
Tharun R <123483287+tharunR-17@users.noreply.github.com>
2025-12-26 11:16:16 +0530

    Create Dungeon Escape: The Path of Keys.cpp

---
+++ Dungeon Escape: The Path of Keys.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MOD = 1e9 + 7;
+
+int countWays(int n) {
+    if (n == 0 || n == 1) return 1;
+
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    cout << countWays(n) << endl;
+    
+    return 0;
+}
